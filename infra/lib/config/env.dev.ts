54d6bb8eabb62c2235c4d2e414ca75eb46c59cfb
Developer <developer@airline-voice-agent.dev>
2026-05-05 21:11:00 -0500

    chore: set dev account ID and deploy networking stack

--- infra/lib/config/env.dev.ts
+++ infra/lib/config/env.dev.ts
@@ -2,7 +2,7 @@ import { EnvironmentConfig } from './schema';
 
 export const devConfig: EnvironmentConfig = {
   environmentName: 'dev',
-  account: process.env.CDK_DEFAULT_ACCOUNT || '000000000000',
+  account: process.env.CDK_DEFAULT_ACCOUNT || '263611243147',
   region: 'us-east-1',
   vpcCidr: '10.0.0.0/16',
   maxAzs: 2,
